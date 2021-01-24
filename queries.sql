88a159235ac03b41fbae5e68a9e34be1ca838676
Wengi <74740339+wengio3@users.noreply.github.com>
2021-01-23 19:32:44 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -44,3 +44,33 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- See retirement_info table
 SELECT * FROM retirement_info;
+
+-- Drop the current retirement_info table
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
